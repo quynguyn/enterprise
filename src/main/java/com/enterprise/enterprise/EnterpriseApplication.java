83b42bd5dba38df0646707b66c8d63516c5b1f16
Des117 <tugiadinh117@gmail.com>
2023-05-02 14:25:32 +0700

    Add some comment

--- src/main/java/com/enterprise/enterprise/EnterpriseApplication.java
+++ src/main/java/com/enterprise/enterprise/EnterpriseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EnterpriseApplication {
 
 	public static void main(String[] args) {
+		// comment
 		SpringApplication.run(EnterpriseApplication.class, args);
 	}
 
