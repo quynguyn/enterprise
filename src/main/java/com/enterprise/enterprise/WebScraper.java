a95d14c2a0411a6eab7f7f0917d08c22e6ccd767
yuhneb1307 <s3878686@rmit.edu.vn>
2023-05-17 22:56:04 +0700

    Add in getPriceTechZones function

--- src/main/java/com/enterprise/enterprise/WebScraper.java
+++ src/main/java/com/enterprise/enterprise/WebScraper.java
@@ -1,5 +1,3 @@
-package com.enterprise.enterprise;
-
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,12 +6,12 @@ import org.jsoup.select.Elements;
 
 public class WebScraper {
     public static void main(String[] args) throws IOException {
-        //initialize();
-        System.out.println(getPriceMemoryZone("NZXT H7 Elite White CM-H71EW-01"));
+        /// initialize();
+        System.out.println(getPriceTechZones("KingMax DDR4 SODIMM"));
     }
 
-    public static void initialize() throws IOException{
-        // Gear VN getting data 
+    public static void initialize() throws IOException {
+        // Gear VN getting data
         // CPU
         getData("https://gearvn.com/collections/cpu-bo-vi-xu-ly?page=");
         getDataTechZone("https://techzones.vn/cpu?pagenumber=");
@@ -23,25 +21,24 @@ public class WebScraper {
         // SSD
         getData("https://gearvn.com/collections/ssd-o-cung-the-ran?page=");
         getDataTechZone("https://techzones.vn/ssd-gan-trong?pagenumber=");
-        //Mother board 
+        // Mother board
         getData("https://gearvn.com/collections/mainboard-bo-mach-chu?page=");
         getDataTechZone("https://techzones.vn/mainboard?pagenumber=");
         // RAM
         getData("https://gearvn.com/collections/ram-pc?page=");
         getDataTechZone("https://techzones.vn/memory-ram?pagenumber=");
-        // Cooler 
+        // Cooler
         getData("https://gearvn.com/collections/fan-rgb-tan-nhiet-pc?page=");
         getDataTechZone("https://techzones.vn/tan-nhiet?pagenumber=");
-        // Power Supply 
+        // Power Supply
         getData("https://gearvn.com/collections/psu-nguon-may-tinh?page=");
         getDataTechZone("https://techzones.vn/psu?pagenumber=");
-        // Case 
+        // Case
         getData("https://gearvn.com/collections/case-thung-may-tinh?page=");
         getDataTechZone("https://techzones.vn/case?pagenumber=");
     }
 
-
-    public static void getData(String urlLink) throws IOException{
+    public static void getData(String urlLink) throws IOException {
         String baseUrl = urlLink;
         int currentPage = 1;
         int totalPages = 1;
@@ -77,16 +74,17 @@ public class WebScraper {
                     productPrice = productPriceElement.text();
                 }
 
-                System.out.println("Name: " + productName + " | Price: " + productPrice+ " | IMG: " + productImage);
+                System.out.println("Name: " + productName + " | Price: " + productPrice + " | IMG: " + productImage);
             }
 
             currentPage++;
         }
     }
 
-    public static String getPriceMemoryZone(String productName) throws IOException{
+    public static String getPriceMemoryZone(String productName) throws IOException {
         String urlName = nameToURL(productName);
-        String urlProduct = "https://memoryzone.com.vn/search?query=" + urlName + "&variantquery=variants_nested.title:(%27%27)%20AND%20variants_nested.variant_available:true";
+        String urlProduct = "https://memoryzone.com.vn/search?query=" + urlName
+                + "&variantquery=variants_nested.title:(%27%27)%20AND%20variants_nested.variant_available:true";
         String price = "";
 
         Document currentPageDoc = Jsoup.connect(urlProduct).get();
@@ -96,17 +94,28 @@ public class WebScraper {
         return price;
     }
 
+    public static String getPriceTechZones(String productName) throws IOException {
+        String urlName = productName.toLowerCase().replaceAll(" ", "-");
+        String urlProduct = "https://techzones.vn/" + urlName;
+
+        Document currentPageDoc = Jsoup.connect(urlProduct).get();
+        Element productRows = currentPageDoc.selectFirst(".price-option");
+        String price = productRows.text();
+
+        return price;
+    }
+
     public static String nameToURL(String name) {
         String url = name;
         url = url.replaceAll(" ", "%20");
         return url;
     }
 
-    public static void getDataTechZone(String urlLink) throws IOException{
+    public static void getDataTechZone(String urlLink) throws IOException {
         String baseUrl = urlLink;
         int currentPage = 1;
         int totalPages = 1;
-    
+
         // Get the total number of pages
         Document firstPage = Jsoup.connect(baseUrl + currentPage).get();
         Element pagination = firstPage.selectFirst(".pagination");
@@ -118,29 +127,29 @@ public class WebScraper {
                 totalPages = Integer.parseInt(lastPageUrlParts[lastPageUrlParts.length - 1]);
             }
         }
-    
+
         // Iterate through all pages
         while (currentPage <= totalPages) {
             Document currentPageDoc = Jsoup.connect(baseUrl + currentPage).get();
             Elements productItems = currentPageDoc.select(".product-item");
-    
+
             // Extract product names and prices and IMG
             for (Element productItem : productItems) {
-                Element productNameElement = productItem.selectFirst(".product-name").selectFirst("div");
+                Element productNameElement = productItem.selectFirst(".product-name").selectFirst("a");
                 String productName = productNameElement.text();
-                Element productPriceElement = productItem.selectFirst(".product-price").selectFirst("a");
+                Element productPriceElement = productItem.selectFirst(".product-price").selectFirst("div");
                 String productPrice = "";
                 Element productImageElement = productItem.selectFirst(".product-img").selectFirst("img");
                 String productImage = productImageElement.attr("src");
                 // System.out.println(productImage);
-    
+
                 if (productPriceElement != null) {
                     productPrice = productPriceElement.text();
                 }
-    
-                System.out.println("Name: " + productName + " | Price: " + productPrice+ " | IMG: " + productImage);
+
+                System.out.println("Name: " + productName + " | Price: " + productPrice + " | IMG: " + productImage);
             }
-    
+
             currentPage++;
         }
     }
